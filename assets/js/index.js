1592acf6a0f578ac75f301436ec8448c0e655d7f
Leslie Owusu-Appiah <leslie@localhost.international>
2023-03-05 21:20:06 +0000

    Updates

---
+++ assets/js/index.js
@@ -0,0 +1,115 @@
+/*
+
+          _             _           _            _              _          _            _            _          
+          _\ \          /\ \        / /\         _\ \           /\ \       /\ \         /\ \         / /\        
+        /\__ \        /  \ \      / /  \       /\__ \          \ \ \     /  \ \       /  \ \       / /  \       
+        / /_ \_\      / /\ \ \    / / /\ \__   / /_ \_\         /\ \_\   / /\ \ \     / /\ \ \     / / /\ \      
+      / / /\/_/     / / /\ \_\  / / /\ \___\ / / /\/_/        / /\/_/  / / /\ \_\   / / /\ \ \   / / /\ \ \     
+      / / /         / /_/_ \/_/  \ \ \ \/___// / /            / / /    / /_/_ \/_/  / / /  \ \_\ / / /  \ \ \    
+    / / /         / /____/\      \ \ \     / / /            / / /    / /____/\    / / /   / / // / /___/ /\ \   
+    / / / ____    / /\____\/  _    \ \ \   / / / ____       / / /    / /\____\/   / / /   / / // / /_____/ /\ \  
+  / /_/_/ ___/\ / / /______ /_/\__/ / /  / /_/_/ ___/\ ___/ / /__  / / /______  / / /___/ / // /_________/\ \ \ 
+  /_______/\__\// / /_______\\ \/___/ /  /_______/\__\//\__\/_/___\/ / /_______\/ / /____\/ // / /_       __\ \_\
+  \_______\/    \/__________/ \_____\/   \_______\/    \/_________/\/__________/\/_________/ \_\___\     /____/_/
+
+  LeslieOA
+
+  Hello again human.
+  The JavaScript used on this website is also human readable.
+
+  Spotted a bug or wanted to ask a question?
+
+  * Visit https://github.com/LeslieOA/leslieoa.github.io/issues
+  * ...or email: leslieoa@pm.me
+
+  Happy hacking. 
+
+*/
+
+
+
+const body = document.body
+const mainContainer = document.querySelector('main')
+
+const domainURI = `${window.top.location.host.toString()}`
+
+const internalLinksRegex = `a[href^="${domainURI}"], a[href^="/"]`
+const anchorLinks = document.querySelectorAll(internalLinksRegex)
+
+const transition = {
+  exit: 'fade-out',
+  enter: 'fade-in',
+}
+
+
+function load(url) {
+  fetch(url)
+  .then((resp) => {
+    resp.text().then((content) => {
+      render({ content, url, xhr: true })
+    })
+  })
+}
+
+
+function render(opts) {
+  const content = opts.content
+  const url = opts.url
+  const xhr = opts.xhr
+  body.classList.add(transition.exit)
+  const fadeAnimation = document.querySelector('body')
+  fadeAnimation.addEventListener('animationend', handleAnimation, true)
+  function handleAnimation(evt) {
+    if (evt.animationName === transition.exit) {
+      fadeAnimation.removeEventListener('animationend', handleAnimation, true)
+      body.classList.remove(transition.exit)
+      if (xhr) {
+        const source = 'main', target = 'main'
+        const targetContainer = document.querySelector(target)
+        const partialTemp = document.createElement('div')
+        partialTemp.innerHTML = content
+        const partial = partialTemp.querySelector(source)
+        targetContainer.replaceWith(partial)
+        history.pushState({ content: partial.innerHTML }, '', url)
+      }
+      else {
+        const mainContainer = document.querySelector('main')
+        mainContainer.innerHTML = content  
+        body.classList.add(transition.enter)
+      }
+      const linkContainer = document.querySelector('main')
+      addHyperlinks(linkContainer.querySelectorAll(internalLinksRegex))
+      body.classList.add(transition.enter)
+    }
+  }
+}
+
+
+function addHyperlinks(hyperlinks) {
+  hyperlinks.forEach((link) => {
+    function handleLink(evt) {
+      evt.preventDefault()
+      const url = link.href
+      load(url)
+    }
+    link.removeEventListener('click', handleLink, true)
+    link.addEventListener('click', handleLink, true)
+  })  
+}
+
+
+window.onload = () => {
+  history.replaceState(
+    { content: mainContainer.innerHTML }, 
+    '', 
+    window.location.toString()
+  )
+  addHyperlinks(anchorLinks)
+}
+
+
+window.onpopstate = function(evt) {
+  body.classList.add('fade-out')
+  var state = evt.state
+  if (state) render({ content: state.content, url: null, xhr: false })
+}
