aefe48c9bf5f2fbb15157222e61bb5c409a1dcec
Leslie Owusu-Appiah <leslie@localhost.international>
2023-03-06 11:15:19 +0000

    Updates

--- assets/js/index.js
+++ assets/js/index.js
@@ -26,90 +26,82 @@
 
 */
 
+const body = document.body;
+const mainContainer = document.querySelector("main");
 
+const domainURI = `${window.top.location.host.toString()}`;
 
-const body = document.body
-const mainContainer = document.querySelector('main')
-
-const domainURI = `${window.top.location.host.toString()}`
-
-const internalLinksRegex = `a[href^="${domainURI}"], a[href^="/"]`
-const anchorLinks = document.querySelectorAll(internalLinksRegex)
+const internalLinksRegex = `a[href^="${domainURI}"], a[href^="/"]`;
+const anchorLinks = document.querySelectorAll(internalLinksRegex);
 
 const transition = {
-  exit: 'fade-out',
-  enter: 'fade-in',
-}
-
+  exit: "fade-out",
+  enter: "fade-in",
+};
 
 function load(url) {
-  fetch(url)
-  .then((resp) => {
+  fetch(url).then((resp) => {
     resp.text().then((content) => {
-      render({ content, url, xhr: true })
-    })
-  })
+      render({ content, url, xhr: true });
+    });
+  });
 }
 
-
 function render(opts) {
-  const content = opts.content
-  const url = opts.url
-  const xhr = opts.xhr
-  body.classList.add(transition.exit)
-  const fadeAnimation = document.querySelector('body')
-  fadeAnimation.addEventListener('animationend', handleAnimation, true)
+  const content = opts.content;
+  const url = opts.url;
+  const xhr = opts.xhr;
+  body.classList.add(transition.exit);
+  const fadeAnimation = document.querySelector("body");
+  fadeAnimation.addEventListener("animationend", handleAnimation, true);
   function handleAnimation(evt) {
     if (evt.animationName === transition.exit) {
-      fadeAnimation.removeEventListener('animationend', handleAnimation, true)
-      body.classList.remove(transition.exit)
+      fadeAnimation.removeEventListener("animationend", handleAnimation, true);
+      body.classList.remove(transition.exit);
       if (xhr) {
-        const source = 'main', target = 'main'
-        const targetContainer = document.querySelector(target)
-        const partialTemp = document.createElement('div')
-        partialTemp.innerHTML = content
-        const partial = partialTemp.querySelector(source)
-        targetContainer.replaceWith(partial)
-        history.pushState({ content: partial.innerHTML }, '', url)
+        const source = "main",
+          target = "main";
+        const targetContainer = document.querySelector(target);
+        const partialTemp = document.createElement("div");
+        partialTemp.innerHTML = content;
+        const partial = partialTemp.querySelector(source);
+        targetContainer.replaceWith(partial);
+        history.pushState({ content: partial.innerHTML }, "", url);
+      } else {
+        const mainContainer = document.querySelector("main");
+        mainContainer.innerHTML = content;
+        body.classList.add(transition.enter);
       }
-      else {
-        const mainContainer = document.querySelector('main')
-        mainContainer.innerHTML = content  
-        body.classList.add(transition.enter)
-      }
-      const linkContainer = document.querySelector('main')
-      addHyperlinks(linkContainer.querySelectorAll(internalLinksRegex))
-      body.classList.add(transition.enter)
+      const linkContainer = document.querySelector("main");
+      addHyperlinks(linkContainer.querySelectorAll(internalLinksRegex));
+      body.classList.add(transition.enter);
     }
   }
 }
 
-
 function addHyperlinks(hyperlinks) {
   hyperlinks.forEach((link) => {
     function handleLink(evt) {
-      evt.preventDefault()
-      const url = link.href
-      load(url)
+      evt.preventDefault();
+      const url = link.href;
+      load(url);
     }
-    link.removeEventListener('click', handleLink, true)
-    link.addEventListener('click', handleLink, true)
-  })  
+    link.removeEventListener("click", handleLink, true);
+    link.addEventListener("click", handleLink, true);
+  });
 }
-
 
 window.onload = () => {
   history.replaceState(
-    { content: mainContainer.innerHTML }, 
-    '', 
+    { content: mainContainer.innerHTML },
+    "",
     window.location.toString()
-  )
-  addHyperlinks(anchorLinks)
-}
+  );
+  addHyperlinks(anchorLinks);
+};
 
-
-window.onpopstate = function(evt) {
-  body.classList.add('fade-out')
-  var state = evt.state
-  if (state) render({ content: state.content, url: null, xhr: false })
-}
+window.onpopstate = function (evt) {
+  body.classList.add("fade-out");
+  var state = evt.state;
+  if (state) render({ content: state.content, url: null, xhr: false });
+};
